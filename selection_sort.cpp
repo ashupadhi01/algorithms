9695c32f3ccc32d4abebfe61c0f8a9b941e50600
Ashutosh Padhi <ashutoshpadhi220@gmail.com>
2023-06-22 21:43:14 +0530

    written code of selection sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,6 +1,14 @@
 void selection_sort(vector <int>& v) {
+    int temp;
   
-
-
-
+    for(int i = 0; i < v.size() - 1; i++) {
+      for(int j = i + 1; j < v.size(); j++) {
+          if(v[i] > v[j]) {
+            temp = v[j];
+            v[j] = v[i];
+            v[i] = temp;
+          }
+      }
+    }
+  
 }
